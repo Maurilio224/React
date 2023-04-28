c3e64534ecd30f803f6b1fdf60ffe503ea0df448
Maurilio Farina <farinamaurilio93@gmail.com>
2023-04-28 17:52:54 +0200

    esercizio-11:complete

--- main/src/Age.js
+++ main/src/Age.js
@@ -1,10 +1,12 @@
 
 
 export default function Age (props){
-   
+   if(props.age > 18 && props.age < 65){
      return(
        <><p>Your age is {props.age}</p></>
     ) 
+   } 
+
  }
 
     
--- main/src/App.js
+++ main/src/App.js
@@ -2,15 +2,13 @@ import Age from "./Age";
 import Welcome from "./Welcome";
 
 function App(props) {
-  if(props.age){return (
+  return (
     <>
-    <Welcome name = {"Maurilio"} />
-    <Age age ={18}/>
+      <Welcome name={"Maurilio"} />
+      <Age age={64} />
     </>
-  );}else{return(
-    <><Welcome name = {"Maurilio"} /></>
-  )}
-  
+  );
+
 }
 
 export default App;
