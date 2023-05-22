17680a80c627eafd611fd40640001b36cda26528
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 07:03:15 +0200

    esercizio-39:complete

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
-import {Counter} from "./Counter";
+import React from "react";
+import { useDataInputs } from "./useDataInputs";
 
 export function App() {
 
+    const { onLogin , onUser , onPass } = useDataInputs()
 
-    return <Counter />
+    return (
+        <>
+        <form>
+            <input onChange={onUser}></input>
+            <input type="password" onChange={onPass}></input>
+            <button onClick={onLogin} type="submit">Login</button>
+        </form>
+        </>
+    )
 }
