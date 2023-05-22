94b2b450ed6cbf6180c20334ebced2bb9b9a5738
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 06:35:27 +0200

    esercizio-33:complete

--- main/src/index.js
+++ main/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ClickCounter from './ClickCounter';
+import ReactDOM, { createRoot } from 'react-dom/client';
+import Input from './Input';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <ClickCounter/>
+ <Input/>
 );
