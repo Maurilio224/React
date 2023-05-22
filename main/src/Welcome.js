ef007cde1036d1e2c2fc40ba91f906b2b0dab151
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 05:11:23 +0200

    esercizio-21:complete

--- main/src/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import InteractiveWelcome from "./InteractiveWelcome";
-
-export default class Welcome extends React.Component {
-    render() {
-        return <p>Welcome, {this.props.name} </p>
-    }
-}
--- main/src/index.js
+++ main/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import InteractiveWelcome from './InteractiveWelcome';
+import Input from './Input';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <InteractiveWelcome/>
+ <Input/>
 );
