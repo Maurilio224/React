6a8a7166b38e3bb797674a9b8c92c95bf3de3be4
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 07:52:30 +0200

    esercizio-44:complete

--- main/src/App.jsx
+++ main/src/App.jsx
@@ -1,26 +1,8 @@
 import React, { useState } from "react";
-import { LanguageContext } from "./LanguageContext";
-import DisplayLanguages from "./DisplayLanguages"
+import {Counter} from "./Counter";
+
+export function App() {
 
 
-
-export default function App() {
-
-    const [language, setLanguage] = useState('en')
-
-    function handleChangeLanguage(event) {
-        setLanguage({language : event.target.value})    
-    }
-
-        return (
-            <div>
-            <select value={language} onChange={handleChangeLanguage}>
-                <option value="en">english</option>
-                <option value="it">italiano</option>
-            </select>
-            <LanguageContext.Provider value={language}>
-                <DisplayLanguages/>
-            </LanguageContext.Provider>
-            </div>
-        )
+    return <Counter />
 }
