62655e5e7588451845e6f47d47e30032991675c2
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 06:19:38 +0200

    esercizio-30:complete

--- main/src/App.js
+++ main/src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
-import TodoList from "./TodoList"
+import { LanguageContext } from "./LanguageContext";
+import DisplayLanguages from "./DisplayLanguages"
 
 
-export default function App() {
-    const renderrArr = (arr, remove) => {
-        return arr.map((item, index) => {
+
+export default class App extends React.Component {
+    
+    state = {
+        language : "en",
+
+    }
+
+     handleChangeLanguage = (event) => {
+         this.setState = {
+            language : event.target.value
+         }
+    }
+
+    render() {
         return (
-            <> 
-            <li index={index}>{item}</li> 
-            <button onClick={() => remove(index)}>delete</button>
-            </>
-            )})
-    } 
-     return <TodoList render={renderrArr}/>
+            <div>
+            <select value={this.state.language} onChange={this.handleChangeLanguage}>
+                <option value="en">english</option>
+                <option value="it">italiano</option>
+            </select>
+            <LanguageContext.Provider value={this.state.language}>
+                <DisplayLanguages/>
+            </LanguageContext.Provider>
+            </div>
+        )
+    }
 }
---
+++ main/src/DisplayLanguages.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { LanguageContext } from "./LanguageContext";
+
+// const SayHello = {
+//   en: {
+//     SAY_HELLO: "hello world",
+//   },
+//   it: {
+//     SAY_HELLO: "ciao mondo",
+//   },
+// };
+
+export default class DisplayLanguages extends React.Component {
+  render() {
+    return <h1>Hello world</h1>
+  }
+}
