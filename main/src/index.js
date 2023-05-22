433afc8dbe378a60404718c79a03629280e45469
Maurilio Farina <farinamaurilio93@gmail.com>
2023-05-22 06:40:26 +0200

    esercizio-34:complete

--- main/src/index.js
+++ main/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
-import Input from './Input';
-
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Input/>
+ <App/>
 );
